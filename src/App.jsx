750a3bee6371c5bff8bfa4cbc6ea3917542ed52e
rachel873 <racheliradukunda873@gmail.com>
2025-03-03 12:44:06 +0200

    simple starting for the bignner

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Pro from "./props"
+import Counter from "./state";
+function App(){
+  return(
+    <div>
+    <Pro name ="kevinnpro"/>
+    <Counter/>
+    </div>
+  )
+}
+export default App;
---
+++ src/props.jsx
@@ -0,0 +1,9 @@
+function Pro(props){
+    return(
+        <div>
+            <h1>Hello There👋  kevin cyubahiro</h1>
+            <p>I'm:   {props.name}</p>
+        </div>
+    )
+}
+export default Pro;
